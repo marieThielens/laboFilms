41efbe5c621b4ad7bcb19184a000693301d906d5
marieThielens <thielens.marie@gmail.com>
2021-07-19 02:56:03 -0700

    recherche input

--- src/conteneur/Recherche.js
+++ src/conteneur/Recherche.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import styleGlobal from '../styleGlobal';
 
 const Recherche = () => {
     return(
+        <View>
+        <Text style={styleGlobal.titre}>Trouver un film</Text>
+        <TextInput style={styleGlobal.input} placeholder='Titre du film' value={inputCherche} onChangeText={(text) => setInputCherche(text)} />
+        <Button title='Rechercher' onPress={montrerFilms}/>
+        </View>
 
     )
 }
