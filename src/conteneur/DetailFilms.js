d6b8368a164302db4766ffae0501c2e768a7fed9
marieThielens <thielens.marie@gmail.com>
2021-07-20 01:14:34 -0700

    clavier se retire

--- src/conteneur/DetailFilms.js
+++ src/conteneur/DetailFilms.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, KeyboardAvoidingView, TouchableWithoutFeedback, Keyboard } from 'react-native';
 import styleGlobal from '../styleGlobal';
 
 import { imageApi } from '../api/FilmApi'; // Récupérer les images des films avec l'Api
@@ -8,25 +8,29 @@ const DetailFilm = ( film ) => {
   //  console.log(props);  //envoie tout le contenu de filmData
     //const { film } = props;
     return (
-        <View style={styleGlobal.film_container}>
-            
-            <Image style={styleGlobal.image} source={{uri: imageApi(film.mesFilms.poster_path)}} />
-            <View style={styleGlobal.content_container}>
-                <View style={styleGlobal.header_container}>
-                    {/* props. mesFilms voir flatlist dans */}
-                    <Text style={styleGlobal.title_text}>{film.mesFilms.title}</Text>
-                    <Text style={styleGlobal.vote_text}>{ film.mesFilms.vote_average}</Text>
+        <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"}>
+            <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+                <View style={styleGlobal.film_container}>
+                    
+                    <Image style={styleGlobal.image} source={{uri: imageApi(film.mesFilms.poster_path)}} />
+                    <View style={styleGlobal.content_container}>
+                        <View style={styleGlobal.header_container}>
+                            {/* props. mesFilms voir flatlist dans */}
+                            <Text style={styleGlobal.title_text}>{film.mesFilms.title}</Text>
+                            <Text style={styleGlobal.vote_text}>{ film.mesFilms.vote_average}</Text>
+                        </View>
+                        <View style={styleGlobal.description_container}>
+                            <Text style={styleGlobal.description_text} numberOfLines={6}>{film.mesFilms.overview}</Text>
+                            {/* La propriété numberOfLines permet de couper un texte si celui-ci est trop long, il suffit de définir un nombre maximum de ligne */}
+                        </View>
+                        
+                        <View style={styleGlobal.date_container}>
+                            <Text style={styleGlobal.date_text}>{film.mesFilms.release_date}</Text>
+                        </View>
+                    </View>
                 </View>
-                <View style={styleGlobal.description_container}>
-                    <Text style={styleGlobal.description_text} numberOfLines={6}>{film.mesFilms.overview}</Text>
-                    {/* La propriété numberOfLines permet de couper un texte si celui-ci est trop long, il suffit de définir un nombre maximum de ligne */}
-                </View>
-                
-                <View style={styleGlobal.date_container}>
-                    <Text style={styleGlobal.date_text}>{film.mesFilms.release_date}</Text>
-                </View>
-            </View>
-        </View>
+            </TouchableWithoutFeedback>
+        </KeyboardAvoidingView>
     )
 }
 
